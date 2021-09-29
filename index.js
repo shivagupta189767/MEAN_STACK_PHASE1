f547ac0e118d45c0bd49c897cbe8a59557963f4a
Shiva <lekhshiva017@gmail.com>
2021-09-29 18:13:27 +0530

    version3

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 function editIconHandler(){
     console.log('a');
 }
+var title ='';
+var content = '';
+var author = '';
+function titleHandler(event){
+    title = event.target.value;
+}
+
+function contentHandler(event){
+    content = event.target.value;
+}
+
+function authorHandler(event){
+    author = event.target.value;
+}
+
+function saveHandler(){
+    
+}
